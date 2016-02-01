3b740f4444ead190af2bed625ab646d2ab24aa04
Shafrai <shafray@i.ua>
2016-02-01 17:56:06 +0200

    Add events.js

---
+++ events.js
@@ -0,0 +1,79 @@
+"use strict";
+var eventMixin = {
+
+  /**
+   * Подписка на событие
+   * Использование:
+   *  menu.on('select', function(item) { ... }
+  */
+  on: function(eventName, handler) {
+    if (!this._eventHandlers) this._eventHandlers = {};
+    if (!this._eventHandlers[eventName]) {
+      this._eventHandlers[eventName] = [];
+    }
+    this._eventHandlers[eventName].push(handler);
+  },
+
+  /**
+   * Прекращение подписки
+   *  menu.off('select',  handler)
+   */
+  off: function(eventName, handler) {
+    var handlers = this._eventHandlers && this._eventHandlers[eventName];
+    if (!handlers) return;
+    for(var i=0; i<handlers.length; i++) {
+      if (handlers[i] == handler) {
+        handlers.splice(i--, 1);
+      }
+    }
+  },
+
+  /**
+   * Генерация события с передачей данных
+   *  this.trigger('select', item);
+   */
+  trigger: function(eventName /*, ... */) {
+
+    if (!this._eventHandlers || !this._eventHandlers[eventName]) {
+      return; // обработчиков для события нет
+    }
+
+    // вызвать обработчики
+    var handlers = this._eventHandlers[eventName];
+    for (var i = 0; i < handlers.length; i++) {
+      handlers[i].apply(this, [].slice.call(arguments, 1));
+    }
+
+  }
+};
+
+function Menu() {
+  // ...
+}
+
+for(var key in eventMixin) {
+  Menu.prototype[key] = eventMixin[key];
+}
+
+// Генерирует событие select при выборе значения
+Menu.prototype.choose = function(value) {
+  this.trigger("select", value);
+}
+
+// Создадим меню
+var menu = new Menu();
+
+// При наступлении события select вызвать эту функцию
+menu.on("select", function(value) {
+  console.log("Выбрано значение " + value);
+});
+
+menu.on("select", function z(value) {
+  console.log("Выбрано значение 2 " + value);
+  if (value==2){
+    menu.off("select", z);
+  }
+});
+
+// Запускаем выбор (событие select вызовет обработчики)
+setInterval(()=> menu.choose(Math.round(Math.random() * 100)), 1000);
